c059af55e38fe48bf1bca98629abf10f5aeb9805
kerryz <kerryzhang92@gmail.com>
2016-01-18 01:07:07 +0100

    Trying to get_latent_order(Z, Zmodel) using similarity list, init

--- gfa_W_projections.py
+++ gfa_W_projections.py
@@ -7,6 +7,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import gamma
 from scipy.spatial import distance
+from sklearn.decomposition import FactorAnalysis
+
+from collections import Counter
+
 from src.gfa import gfa
 from src.gfa import gfa_experiments
 
@@ -19,6 +23,8 @@ K = 7  # num latent factors
 
 def main():
     W, Z, Y = generate_data()
+    # for FA
+    Y_hstack = np.hstack([Y[m] for m in range(M)])
 
     # Plot true latent components
     for k in range(K):
@@ -36,14 +42,26 @@ def main():
     plt.suptitle("Observations of group 2 (5-dimensional)")
     """
 
+    # GFA
     model = gfa_experiments(Y, K=8, Nrep=10, rotate=False, verbose=1)
+    # FA
+    fa = FactorAnalysis(K)
+    fa.fit(Y_hstack)
 
-    # plot estaimated latent components
+    # plot estaimated latent components from GFA
     plt.figure()
     for k in range(model['K']):
         plt.subplot(model['K'], 1, k+1)
         plt.scatter(range(N), model['Z'][:, k], facecolors='none')
-    plt.suptitle("Estimated active latent components")
+    plt.suptitle("GFA: estimated active latent components")
+
+    # plot estaimated latent components from FA
+    plt.figure()
+    Z_fa = fa.transform(Y_hstack)
+    for k in range(model['K']):
+        plt.subplot(model['K'], 1, k+1)
+        plt.scatter(range(N), Z_fa[:, k], facecolors='none')
+    plt.suptitle("FA: estimated active latent components")
 
     # reorder estimated latent variables to correspond to the true order
 
@@ -56,31 +74,39 @@ def main():
     # corresponds to the true latent variable number y
     order_map = [None]*K
     for k in range(K):
-        w_k_model = Wmodel_conc[k]
-        similarities = [distance.cosine(np.abs(w_k_model), np.abs(W_conc[i])) for i in range(K)]
+        w_k = W_conc[k]
+        similarities = [distance.cosine(np.abs(w_k), np.abs(Wmodel_conc[i])) for i in range(K)]
         most_sim_idx = np.argmin(similarities)
-        print(similarities)
         order_map[k] = most_sim_idx
-    print(order_map)
+
+    ###################
+    # TODO: get reordering from Z, not W
+    order_map_Z = [None]*K
+    for k in range(K):
+        z_k = Z[:, k]
+        similarities = [distance.cosine(z_k, model['Z'][:, i]) for i in range(K)]
+        print(similarities)
+        most_sim_idx = np.argmin(similarities)
+        order_map_Z[k] = most_sim_idx
+    print(order_map_Z)
+    ###################
+
+    Wmodel_ordered = np.array(Wmodel_conc)[order_map].T
     plt.figure()
-    plt.subplot(2, 1, 1)
-    plt.imshow(np.abs(W_conc), cmap=plt.cm.gray_r, interpolation='none')
-    plt.subplot(2, 1, 2)
-    plt.imshow(np.abs(Wmodel_conc), cmap=plt.cm.gray_r, interpolation='none')
-    plt.title("W_model concatenate")
-    plt.show()
+    plt.imshow(np.abs(Wmodel_ordered), cmap=plt.cm.gray_r, interpolation='none')
 
     # plot true matrix projections
-    plot_W(W, "True")
+    plt.figure()
+    plot_grouped_W(W, "True")
 
     plt.figure()
     # plot estimated matrix projections
-    plot_W(model['W'], "Estimated")
+    #plot_grouped_W(model['W'], "Estimated")
 
     plt.show()
 
 
-def plot_W(W, title):
+def plot_grouped_W(W, title):
     M = len(W)
     for m, W_m in enumerate(W):
         plt.subplot(M, 1, m+1)
@@ -94,6 +120,43 @@ def plot_W(W, title):
             labelbottom='off')  # labels along the bottom edge are off
         # plt.colorbar()
     plt.suptitle(title)
+
+
+def get_latent_order(Z, Zmodel):
+    assert(np.all(Z.shape == Zmodel.shape))
+    K = Z.shape[1]
+    # z_similarities[k] = list of tuples, sorted from most similar modeled latent factor index to the least similar
+    # z_similarities[k] = [(z_model_idx, similarity)]
+    z_similarities = [None]*K
+    # z_closest_idx[k] = list of estimated latent factor indices that are closest to the true z_k, sorted from closest to furthest
+    z_closest_idx = [None]*K
+    # z_closest_sim[k] = list of distances for corresponding indices in z_closest_idx
+    z_closest_sim = [None]*K
+    for k in range(K):
+        z_k = Z[:, k]
+        similarities = [distance.cosine(z_k, Zmodel[:, i]) for i in range(K)]
+        sim_argsort = np.argsort(similarities)
+        z_similarities[k] = zip(sim_argsort, similarities[sim_argsort])
+    # order_map[k] = i means that true latent variable number k
+    # corresponds to the estimated latent variable number i
+    order_map = [None]*K
+    for trial in range(K):
+        # the closest index for this trial, but do not include z_k's closest if z_k has already been assigned a model latent factor
+        closest_idx_trial = [None]*K
+        closest_sim_trial = [None]*K
+        for k, mapped in enumerate(order_map):
+            if mapped is None:
+                closest_idx_trial[k] = z_similarities[k][trial][0]
+                closest_sim_trial[k] = z_similarities[k][trial][1]
+        # closest_idx_trial = [z_similarities[k][trial][0] for k in range(K)]
+        # closest_sim_trial = [z_similarities[k][trial][1] for k in range(K)]
+        idx_counter = Counter(closest_idx_trial)
+        idx_counter.pop(None, None)
+        for k, closest_idx in enumerate(closest_idx_trial):
+            # set z_k's closest if not already set and closest_idx is unique for this trial
+            if closest_idx is not None and idx_counter[closest_idx] == 1:
+                order_map[k] = closest_idx
+
 
 
 def generate_data():
